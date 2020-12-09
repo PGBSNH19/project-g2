92f27e6bfdb4b311f123093726812cc56496cbd0
Benjamin Ytterström <benjamin.ytterstrom@learnet.se>
2020-12-09 11:19:59 +0100

    Added Entity Framework to API-projekt
    + Added base entity
    + Added user
    + Added user table

---
+++ KNet/KNet.API/Context/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using KNet.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace KNet.API.Context
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration config, DbContextOptions<AppDbContext> options) : base(options)
+        {
+            _configuration = config;
+        }
+
+        public AppDbContext() 
+        { 
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- KNet/KNet.API/Startup.cs
+++ KNet/KNet.API/Startup.cs
@@ -1,15 +1,9 @@
+using KNet.API.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KNet.API
 {
@@ -25,8 +19,8 @@ namespace KNet.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddDbContext<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
