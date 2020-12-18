51f7ff6adf61acf44ae537e37fcbad92e6a979c9
AntonNorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-12-18 11:29:45 +0100

    [G2-139] Create base repository - Added base repository with interface

---
+++ KNet/KNet.API/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KNet.API.Repository
+{
+    public interface IRepository
+    {
+        Task<T> Add<T>(T entity) where T : class;
+        Task<T> Delete<T>(Guid id) where T : class;
+        Task<bool> Save();
+        Task<T> Update<T>(T entity) where T : class;
+    }
+}
---
+++ KNet/KNet.API/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using KNet.API.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNet.API.Repository
+{
+    public class Repository : IRepository
+    {
+        protected readonly AppDbContext _context;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> Add<T>(T entity) where T : class
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<T> Delete<T>(Guid id) where T : class
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+            {
+                return entity;
+            }
+
+            _context.Set<T>().Remove(entity);
+            await Save();
+            return entity;
+        }
+
+        public async Task<bool> Save()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<T> Update<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await Save();
+            return entity;
+        }
+
+
+    }
+}
