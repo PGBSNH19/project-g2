c19baa81e4ba79d073db71fbc00209393bf916f7
AntonNorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-12-18 13:37:36 +0100

    [G2-46] UserRepository - cleaned up using statements

--- KNet/KNet.API/Repositories/UserRepository.cs
+++ KNet/KNet.API/Repositories/UserRepository.cs
@@ -1,9 +1,7 @@
 ﻿using KNet.API.Context;
 using KNet.API.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
