d5c76e968bba04707e76b5112f20ee96e60bf38d
Benjamin Ytterström <benjamin.ytterstrom@learnet.se>
2020-12-18 11:47:02 +0100

    [G2-33] API.Context - Started with DbInitializer

---
+++ KNet/KNet.API/Context/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNet.API.Context
+{
+    public class DbInitializer
+    {
+        public void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (true)
+            {
+
+            }
+        }
+    }
+}
