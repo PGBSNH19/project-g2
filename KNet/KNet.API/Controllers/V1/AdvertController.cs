b67cd29684b0428aadbcce189bde27b0ca5e03cd
AntonNorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-12-21 15:42:32 +0100

    [G2-64] API.Controllers - Added AdvertController with CRUD

---
+++ KNet/KNet.API/Controllers/V1/AdvertController.cs
@@ -0,0 +1,71 @@
+﻿using KNet.API.Models;
+using KNet.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNet.API.Controllers.V1
+{
+    [Route("api/v1/[controller]")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertRepository _advertRepository;
+        
+        public AdvertController(IAdvertRepository advertRepository)
+        {
+            _advertRepository = advertRepository;
+        }
+        
+        [HttpGet("id")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest();
+
+            var advert = await _advertRepository.GetAdvertById(id);
+
+            if (advert is null)
+                return BadRequest();
+
+            return Ok(advert);
+        }
+
+        [HttpDelete]
+        public async Task <IActionResult> Delete(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest();
+
+            var advert = await _advertRepository.GetAdvertById(id);
+            advert.IsDeleted = true;
+            await _advertRepository.Delete(advert);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Advert request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var advert = await _advertRepository.GetAdvertById(request.Id);
+
+            if (advert is null)
+                return BadRequest();
+
+            advert.CategoryId = request.CategoryId;
+            advert.Heading = request.Heading;
+            advert.Content = request.Content;
+            advert.Location = request.Location;
+            advert.Price = request.Price;
+            advert.StartDate = request.StartDate;
+            advert.EndDate = request.EndDate;
+
+            await _advertRepository.Update(advert);
+            return Ok();
+        }
+    }
+}
--- KNet/KNet.API/Models/Advert.cs
+++ KNet/KNet.API/Models/Advert.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KNet.API.Models
 {
     public class Advert : Entity
     {
         public Guid UserId { get; set; }
+        [NotMapped]
         public User User { get; set; }
         public Guid CategoryId { get; set; }
+        [NotMapped]
         public Category Category { get; set; }
         public string Heading { get; set; }
         public string Content { get; set; }
