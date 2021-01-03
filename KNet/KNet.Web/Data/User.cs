8b5a82f95fd6d4c5f5fa00e574330599c3b2f304
Gerglamesh <hampe.kjellstrand@gmail.com>
2021-01-03 13:10:06 +0100

    Add some fields for storing and displaying data in ad and also add models for holding said data when retrieved from API

---
+++ KNet/KNet.Web/Data/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KNet.Web.Data
+{
+    public class User : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
